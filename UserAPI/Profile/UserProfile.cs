009bf0b6d343d8a960b538d2c45723854b454e53
dev-guilherme-oliveira <coder.guiliv@outlook.com>
2022-11-27 20:51:32 +0000

    Adicionar arquivos de projeto.

---
+++ UserAPI/Profile/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAPI.Data.Dtos.Usuario;
+using UserAPI.Models;
+
+namespace UserAPI.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+            CreateMap<Usuario, IdentityUser<int>>();
+        }
+    }
+}
